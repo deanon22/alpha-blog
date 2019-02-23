abd31921dceff02604602c3db8294456e62f90df
jason <jdean22@gmail.com>
2019-02-23 15:54:43 -0500

    Add edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,20 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Category was successfully updated"
+            redirect_to categories_path(@category)
+        else
+            render 'edit'
+        end
+    end
+
     def show
         @category = Category.find(params[:id])
         @category_articles = @category.articles.paginate(page: params[:page],per_page: 5)   
@@ -30,7 +44,6 @@ class CategoriesController < ApplicationController
     def category_params
         params.require(:category).permit(:name)
     end
-
 
     def require_admin
         if !logged_in? || (logged_in? and !current_user.admin?)
